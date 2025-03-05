3de72a4600d1856c558b1cc51e205ccbaeacddf6
lato99 <lato18@ku.edu.tr>
2025-03-05 10:01:08 +0300

    small changes

--- Challenge_7_User Card/main.js
+++ Challenge_7_User Card/main.js
@@ -1,12 +1,13 @@
 // Write your JavaScript code here
-$("document").ready(function(){
-    $("#signUpButton").on("click", function () {
-        var userMail = $("#mailInput").val();
-        console.log(userMail)
-        Swal.fire({
-            title: "Signed Up!",
-            text: "Check your mail!" + " " + userMail,
-            icon: "success"
-        });
+$("document").ready(function () {
+    $(".showMore").on("click", function () {
+        $(".extraContent").toggleClass("notSeen");
+        $(".showMore").toggleClass("notSeen");
+        $(".showLess").toggleClass("notSeen");
+    })
+    $(".showLess").on("click", function () {
+        $(".extraContent").toggleClass("notSeen");
+        $(".showMore").toggleClass("notSeen");
+        $(".showLess").toggleClass("notSeen");
     })
 })
